6eee26797bc313c8ab48bd1fc55ffec00c621928
Fabricio Soares <fabriciosoares@live.com>
2016-09-24 16:26:25 -0300

    Initial Commit

---
+++ PacManX/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PacManX
+//
+//  Created by Fabrício Soares on 24/09/16.
+//  Copyright © 2016 Fabrício, Yooh e Wesnydy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
